eea9a5b7424ff24d402852440645ad427bd0464b
JosephinaMeester123 <josephina.meester@student.howest.be>
2022-05-23 09:56:19 +0200

    changed filenames to prepare for counting of AWS files

--- Scripts/create_reference.sh
+++ Scripts/create_reference.sh
@@ -8,7 +8,7 @@ if test -f "$FILE"; then
 fi
 
 echo "filename	autosomal_total_count	average_count	stdev_count" >> /home/jmeester/WGS_Jeannette/Testing/summarycounts.txt
-for filename in /home/jmeester/WGS_Jeannette/Testing/*.count.stdout.*.pbs.biomina.be; do
+for filename in /home/jmeester/Internship/countfiles/*.count; do
 	awk '$1 != "chrY" && $1 != "chrX"' $filename > $filename".autosomal"
         awk '{print $1, $2, $3} ' $filename".autosomal" > /home/jmeester/WGS_Jeannette/Testing/locations.txt
         awk '{print $4} ' $filename".autosomal" | awk '$1 != 0' | awk -v myvar="$filename" '{ total += $1; sumsq+=$1*$1 } END { print myvar, total, total/(NR-1), sqrt(sumsq/(NR-1) - (total/(NR-1))^2)}' >> /home/jmeester/WGS_Jeannette/Testing/summarycounts.txt
@@ -17,14 +17,14 @@ done
 #perform normalization
 totalavg="$(awk '{ total += $2} END { print total/(NR-1)}' /home/jmeester/WGS_Jeannette/Testing/summarycounts.txt)"
 
-for file in /home/jmeester/WGS_Jeannette/Testing/*.count.stdout.*.pbs.biomina.be.autosomal; do
+for file in /home/jmeester/WGS_Jeannette/Testing/*.count.autosomal; do
 	totalsample="$(awk '{total += $4} END {print total}' $file)"
-	awk -v totalavg="$totalavg" -v totalsample="$totalsample" '{print $1, $2, $3, $4, totalsample, totalavg, $4/totalsample*totalavg}' $file > $file".count.norm"
-	awk '{print $7} ' $file".count.norm" > $file".count.norm.countonly"
+	awk -v totalavg="$totalavg" -v totalsample="$totalsample" '{print $1, $2, $3, $4, totalsample, totalavg, $4/totalsample*totalavg}' $file > $file".norm"
+	awk '{print $7} ' $file".norm" > $file".norm.countonly"
 done
 
 #paste together windows1kb file, which contains locations of bins, with the counts of all samples
-paste /home/jmeester/WGS_Jeannette/Testing/locations.txt /home/jmeester/WGS_Jeannette/Testing/*.autosomal.count.norm.countonly > /home/jmeester/Internship/Reference_test/auto_count_allsamples_norm.txt
+paste /home/jmeester/WGS_Jeannette/Testing/locations.txt /home/jmeester/WGS_Jeannette/Testing/*.autosomal.norm.countonly > /home/jmeester/Internship/Reference_test/auto_count_allsamples_norm.txt
 
 #calculate average and standard deviation per row to create a reference set^
 awk 'BEGIN{OFS=","} {T=0; V=0;
@@ -33,7 +33,8 @@ awk 'BEGIN{OFS=","} {T=0; V=0;
 	stdev = sqrt(V/(NF-3));
 	print $1, $2, $3, T, stdev }' /home/jmeester/Internship/Reference_test/auto_count_allsamples_norm.txt > /home/jmeester/Internship/Reference_test/referenceNormals.stats.binned_norm.csv
 
-rm /home/jmeester/WGS_Jeannette/Testing/*.autosomal.count.norm.countonly
+rm /home/jmeester/WGS_Jeannette/Testing/*.autosomal.norm.countonly
+rm /home/jmeester/WGS_Jeannette/Testing/*.autosomal.norm
 rm /home/jmeester/WGS_Jeannette/Testing/*.autosomal
 
 
