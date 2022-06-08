7c1b2309e35f615a7fa6d59f7ed3bcd4880ba0c5
JosephinaMeester123 <josephina.meester@student.howest.be>
2022-06-08 21:41:30 +0200

    updates regarding correct processing of columns and reference

--- Scripts/CNV.Calling.R
+++ Scripts/CNV.Calling.R
@@ -1,9 +1,4 @@
-#load library
-#if (!require("BiocManager", quietly = TRUE))
-#  install.packages("BiocManager")
-
-#BiocManager::install("DNAcopy")
-
+#load libraries
 library(grid)
 library(gridExtra)
 library(lattice)
@@ -11,57 +6,52 @@ library(ggplot2)
 library(DNAcopy)
 options(warn=1)
 
+
+#=====================================================================================================
+
+# ARGUMENT :
+#  1 : input file (counts / bins)
+#  2 : sample name
+#  3 : output folder
+
+# example of arguments:
+# /opt/software/miniconda_rnaseq/bin/Rscript /home/jmeester/Internship/Scripts/CNV.Calling.R /home/jmeester/Internship/countfiles/101557.count.autosomal.norm 101557 /home/jmeester/Internship/CNV/
+
+#=======================================================================================================
 # read arguments
-#args <- commandArgs(TRUE)
-#bin_file <- args[1]
-#samplename <- args[2]
-samplename <- 127869
-#gender <- args[3]
-gender <- "MALE"
-#ref_map <- args[4]
-
-#output_map <- args[5]
-output_map <- "C:/Users/jeann/Desktop/CBS/"
-
-#tresholdadel <- as.numeric(args[6])
-tresholdadel <- as.numeric(-2)
-
-#tresholdadup <- as.numeric(args[7])
-tresholdadup <- as.numeric(2)
-
-#tresholdxdel <- as.numeric(args[8])
-tresholdxdel <- as.numeric(-3)
-
-#tresholdxdup <- as.numeric(args[9])
-tresholdxdup <- as.numeric(3)
-
-#adel <- as.integer(args[10])
+args <- commandArgs(TRUE)
+bin_file <- args[1]
+samplename <- args[2]
+output_map <- args[3]
+tresholdadel <- as.numeric(-4)
+tresholdadup <- as.numeric(3)
 adel <- as.integer(8)
-
-#adup <- as.integer(args[11])
 adup <- as.integer(8)
 
-#xdel <- as.integer(args[12])
-xdel <- as.integer(8)
 
-#xdup <- as.integer(args[13])
-xdup <- as.integer(8)
-
+#bin_file <- "/home/jmeester/Internship/countfiles/101557.count.autosomal.norm"
+#samplename <- 101557
+#output_map <- "/home/jmeester/Internship/CNV/"
+#ref_map <- args[4]
+#tresholdadel <- as.numeric(args[6])
+#tresholdadup <- as.numeric(args[7])
+#adel <- as.integer(args[10])
+#adup <- as.integer(args[11])
 #diagBandcsv <- args[14]
-
 #cytoBandcsv <- args[15]
 
-bintreshold <- matrix(c(adel,adup,xdel,xdup),ncol=2,nrow=2,byrow=T)
+
+bintreshold <- matrix(c(adel,adup),ncol=2,nrow=1,byrow=T)
 #           ||  del  |  dup
 #==========================
 # Autosomen ||  adel | adup
-#           ||-------------
-# Xchrom    ||  xdel | xdup
+
 
 # set parameters
-genders = c('jongen' = 'M', 'meisje' = 'F', 'male' = 'M', 'female' = 'F', 'm' = 'M', 'f' = 'F')
 min.width.bins = 2
 alpha=0.001
+
+#=====================================================================================================
 
 panel.fill<-function(x,y,mychr,CBSsegments){
   normalIndex<-which(abs(y)<10)
@@ -76,11 +66,11 @@ panel.fill<-function(x,y,mychr,CBSsegments){
   
   myLwd = 0.4
   
-  diagnosticBand <- read.csv("C:/Users/jeann/Desktop/diagnosticBand.b37.csv",header=TRUE)
-  cytoBand <- read.csv("C:/Users/jeann/Desktop/cytoBand.b37.csv",header=TRUE)
-  
   cyto.H = 0.10
   cyto.Y = 0.07 #0.85
+  
+  diagnosticBand <- read.csv("/home/jmeester/Internship/diagnosticBand.b37.csv",header=TRUE)
+  cytoBand <- read.csv("/home/jmeester/Internship/cytoBand_hg38.csv",header=TRUE)
   
   cyto.chr <-  subset(cytoBand,cytoBand$CHR==as.character(mychr))
   diag.chr <-  subset(diagnosticBand,diagnosticBand$CHR==as.character(mychr))
@@ -100,13 +90,8 @@ panel.fill<-function(x,y,mychr,CBSsegments){
   }
   
   panel.abline(h=0,col=rgb(0,0,0,0.5),lwd=2) #zero
-  if(mychr == "X"){
-    panel.abline(h=tresholdxdup,col=rgb(0,0,1,0.5),lwd=2,lty=3)   #CNV treshold
-    panel.abline(h=tresholdxdel,col=rgb(0,0,1,0.5),lwd=2,lty=3)
-  } else {
-    panel.abline(h=tresholdadup,col=rgb(0,0,1,0.5),lwd=2,lty=3)   #CNV treshold
-    panel.abline(h=tresholdadel,col=rgb(0,0,1,0.5),lwd=2,lty=3)
-  }
+  panel.abline(h=tresholdadup,col=rgb(0,0,1,0.5),lwd=2,lty=3)   #CNV treshold
+  panel.abline(h=tresholdadel,col=rgb(0,0,1,0.5),lwd=2,lty=3)
 
   for (i in 1:nrow(cyto.chr)) {
     row <- cyto.chr[i,]
@@ -163,55 +148,36 @@ panel.fill<-function(x,y,mychr,CBSsegments){
 }
 
 # read data
+bin_data = read.table(bin_file,as.is=T,header=T,sep=" ")
 #bin_data = read.table(bin_file,as.is=T,header=T,sep=",")
-bin_data <- read.table("C:/Users/jeann/Desktop/127869.count.stdout.5890115.pbs.biomina.be",as.is=T,header=F,sep="\t")
-names(bin_data)[1:4] <- list("CHR", "BIN.START", "BIN.END", "COUNT")
+#bin_data <- read.table("/home/jmeester/Internship/countfiles/106282.count.autosomal.norm",as.is=T,header=F,sep=" ")
+names(bin_data)[1:7] <- list("CHR","BIN.START","BIN.END","RAW.COUNT","sample.total","total.avg","NORM.COUNT")
+#names(bin_data)[1:12] <- list("SAMPLE","CHR","BIN.START","BIN.END","BIN.GC.CONTENT","BIN.N.COUNT","COUNT","GC","GR10M","GC.GR10M","AUTO.GR10M","GC.AUTO.GR10M")
+summary(bin_data)
 
 sample.count <- names(bin_data)[7]
-#sample.gc.auto.X.F.gr10M <- sub("COUNT","GC.AUTO.X.F.GR10M",sample.count)
-#sample.gc.auto.X.M.gr10M <- sub("COUNT","GC.AUTO.X.M.GR10M",sample.count)
-#sample.gc.auto.gr10M <- sub("COUNT","GC.AUTO.GR10M",sample.count)
-
+#sample.gc.auto.gr10M <- sub("COUNT","AUTO.GR10M",sample.count)
 
 # ref data
-## input file is loaded here --> contains header, csv file
 #ref_bin_file = paste(ref_map,"referenceNormals.stats.binned.csv",sep="/");
-ref_data <- read.table("C:/Users/jeann/Desktop/referenceNormals.stats.binned.csv",as.is=T,header=F,sep=",")
-names(ref_data)[1:5] <- list("CHR", "BIN.START", "BIN.END", "COUNT.MEAN", "COUNT.SD")
-
-
-# get gender specific ref data.
-#gender_ref_data <- ref_data;
-#if(!(is.na(genders[tolower(gender)]))){
-#    gender_ref_bin_file <- sub("stats.binned.csv",paste('stats.',genders[tolower(gender)],'.binned.csv',sep=''),ref_bin_file,fixed=TRUE)
-#    gender_ref_data <- read.table(gender_ref_bin_file,as.is=T,header=T,sep=",")
-#} else {
-
-#    message(paste("Using standard reference data for gender specific reference data (unknown gender: ",gender,")",sep=""));
-#    gender_ref_bin_file <- sub("stats.binned.csv",paste('stats.','M','.binned.csv',sep=''),ref_bin_file,fixed=TRUE)
-#    gender_ref_data <- read.table(gender_ref_bin_file,as.is=T,header=T,sep=",")
-#    message(paste("Using standard male data for gender specific reference data for unknown gender: ",gender,sep=""));
-    # reason male ref: 
-    # - figures chr X and Y on report are sample vs male when undetermined
-    # - more likely to have male < gendertreshold than XXY
-#}
+ref_data <- read.table("/home/jmeester/Internship/Reference_test/referenceNormals_norm.csv",as.is=T,header=F,sep=",")
+#ref_data <- read.table("/home/jmeester/Internship/Reference_test/referenceNormals.csv",as.is=T,header=F,sep=",")
+names(ref_data)[1:5] <- list("CHR", "BIN.START", "BIN.END", "COUNT.MEAN","COUNT.SD")
+#names(ref_data)[1:15] <- list("CHR", "BIN.START", "BIN.END", "COUNT.MEAN","COUNT.SD","GC.MEAN","GC.SD","GR10M.MEAN","GR10M.SD","GC.GR10M.MEAN","GC.GR10M.SD","AUTO.GR10M.MEAN","AUTO.GR10M.SD","GC.AUTO.GR10M.MEAN","GC.AUTO.GR10M.SD")
+summary(ref_data)
 
 #autosomes: with normal ref
-#x: with gender ref
-
 achrom = bin_data[which(bin_data$CHR != "chrX" & bin_data$CHR != "chrY"),]	#autosomes
 achrom_full <- merge(achrom,ref_data,by=c('CHR','BIN.START','BIN.END'))
 achrom_full$CHR <- gsub("[a-zA-Z ]", "", achrom_full$CHR)
-
-
-# Y: no CNVcalling
-# sd_full$ratio = sd_full[,16] / sd_full[,35]
-#	sd_full$z = (sd_full[,16] - sd_full[35]) / sd_full[,36]
+summary(achrom)
 
 # no gender data
-achrom_full$ratio = achrom_full[,4] / achrom_full[,5]
-achrom_full$z = (achrom_full[,4] - achrom_full[,5]) / achrom_full[,6]
+achrom_full$ratio = achrom_full[,7] / achrom_full[,8]
+achrom_full$z = (achrom_full[,7] - achrom_full[,8]) / achrom_full[,9]
 achrom_full$logR = log2(achrom_full$ratio)
+summary(achrom_full)
+head(achrom_full)
 
 #browseVignettes("DNAcopy")
 aCNA <- CNA(as.matrix(achrom_full$z),achrom_full$CHR,as.numeric(achrom_full$BIN.START),data.type="logratio",sampleid=samplename)
@@ -221,7 +187,7 @@ aSeg$Sample <- samplename
 
 CNVtable = aSeg
 
-CNVtable$loc.end = CNVtable$loc.end + 49999
+CNVtable$loc.end = CNVtable$loc.end + 499
 
 #add Ratio to table
 tmp = matrix(nrow=0,ncol=8)
@@ -229,11 +195,7 @@ for(i in seq(1,dim(CNVtable)[1])){
   chr = as.character(CNVtable[i,"chrom"])
   start = as.integer(CNVtable[i,"loc.start"])
   end = as.integer(CNVtable[i,"loc.end"])
-  if(chr=="X"){
-    cnv_full = xchrom_full[which(xchrom_full$CHR == chr & xchrom_full$BIN.START >= start & xchrom_full$BIN.END <= end),]
-  } else {
-    cnv_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start & achrom_full$BIN.END <= end),]
-  }
+  cnv_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start & achrom_full$BIN.END <= end),]
   tmp = rbind(tmp,c(as.character(samplename),as.character(chr),start,end,
                     as.integer(dim(cnv_full)[1]),as.integer(dim(cnv_full)[1])-sum(!is.finite(cnv_full$ratio)),
                     as.numeric(round(mean(cnv_full[is.finite(cnv_full$z),"z"],na.rm=T),2)),
@@ -290,27 +252,9 @@ for(chromosome in seq(1,22)){
   dev.off()
 }
 
-if(exists("xObj")){
-  pdf(paste(output_map,paste(samplename,".","chr","X",".CBSplot.pdf",sep=""),sep="/"))
-  xchrom_full$BIN.START.REL = xchrom_full$BIN.START / max(xchrom_full$BIN.START)
-  print(xyplot(xchrom_full$z~xchrom_full$BIN.START.REL,
-         xlab="Position (Mbp)",ylab=list(label="Z-score",y=unit(0.6,"native")),panel=panel.fill,mychr="X",
-         ylim=c(-15,10.2),CBSsegments=CNVtable,
-         strip=F,scales=list(col=1,tck=c(1,0),
-                             y=list(draw=T,at=seq(-10,10,2)),
-                             x=list(draw=T,at=seq(0,1,0.1),labels = round(seq(0,10,1)*max(xchrom_full$BIN.START)/10e6,1))),
-         par.settings=list(strip.background=list(col="grey"), 
-                           layout.heights = nopadding.heights, 
-                           layout.widths = nopadding.widths,
-                           axis.line = list(col=0)),
-         main=paste("CBS-Plot ",samplename,":chrX",sep="")))
-  dev.off()
-}
-
 #FILTER SCORE
 trueCNVs = CNVtable[which(
-                    CNVtable$chrom != "X" & (CNVtable$Z >= tresholdadup | CNVtable$Z <= tresholdadel) |
-                    CNVtable$chrom == "X" & (CNVtable$Z >= tresholdxdup | CNVtable$Z <= tresholdxdel)
+                    CNVtable$chrom != "X" & (CNVtable$Z >= tresholdadup | CNVtable$Z <= tresholdadel)
                     )
                   ,]
 
@@ -329,22 +273,13 @@ while(i <= dim(trueCNVs)[1]){
 }
 
 #FILTER BINS
-GJB6_start = 20750000
-GJB6_end = 21100000
+#GJB6_start = 20750000
+#GJB6_end = 21100000
 
 #FILTER BINS
 trueCNVs = trueCNVs[ which(
   (as.character(trueCNVs$chrom) != "X" & as.numeric(trueCNVs$Z) < 0 & as.numeric(trueCNVs$bins) >= adel) |
-  (as.character(trueCNVs$chrom) != "X" & as.numeric(trueCNVs$Z) > 0 & as.numeric(trueCNVs$bins) >= adup) |
-  (as.character(trueCNVs$chrom) == "X" & as.numeric(trueCNVs$Z) < 0 & as.numeric(trueCNVs$bins) >= xdel) |
-  (as.character(trueCNVs$chrom) == "X" & as.numeric(trueCNVs$Z) > 0 & as.numeric(trueCNVs$bins) >= xdup) |
-  (
-    as.character(trueCNVs$chrom) == "13" &
-    as.numeric(trueCNVs$loc.start) <= GJB6_end &
-    as.numeric(trueCNVs$loc.end) >= GJB6_start &
-    as.numeric(trueCNVs$Z) < 0 &
-    as.numeric(trueCNVs$bins) >= 6
-  )
+  (as.character(trueCNVs$chrom) != "X" & as.numeric(trueCNVs$Z) > 0 & as.numeric(trueCNVs$bins) >= adup)
 ),]
 list=matrix(ncol=7,nrow=0)
 if(dim(trueCNVs)[1]>0){
@@ -352,18 +287,13 @@ if(dim(trueCNVs)[1]>0){
     chr = as.character(trueCNVs[i,"chrom"])
     start = as.integer(trueCNVs[i,"loc.start"])
     end = as.integer(trueCNVs[i,"loc.end"])
-    if(chr=="X"){
-      sd_full = xchrom_full[which(xchrom_full$CHR == chr & xchrom_full$BIN.START >= start - 10*50000  & xchrom_full$BIN.END <= end + 1 + 10*50000),]
-      cnv_full = xchrom_full[which(xchrom_full$CHR == chr & xchrom_full$BIN.START >= start & xchrom_full$BIN.END <= end),]
-    } else {
-      sd_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start - 10*50000  & achrom_full$BIN.END <= end + 1 + 10*50000),]
-      cnv_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start & achrom_full$BIN.END <= end),]
-    }
+    sd_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start - 10*500  & achrom_full$BIN.END <= end + 1 + 10*500),]
+    cnv_full = achrom_full[which(achrom_full$CHR == chr & achrom_full$BIN.START >= start & achrom_full$BIN.END <= end),]
     
     list = rbind(list,c(samplename,chr,paste(start,end,sep="-"),dim(cnv_full)[1],dim(cnv_full)[1]-sum(!is.finite(cnv_full$ratio)),
                         round(mean(cnv_full[is.finite(cnv_full$z),"z"],na.rm=T),2),round(mean(cnv_full[is.finite(cnv_full$ratio),"ratio"],na.rm=T),2)))
     
-    sd_full$pos = sd_full$BIN.START + 25000
+    sd_full$pos = sd_full$BIN.START + 250
     
     dataoutput <- paste(output_map,paste(samplename, paste("chr",chr,sep=""),paste(start,end,sep="-"),'CNVData.csv',sep='.'),sep="/");
     write.table(sd_full,file=dataoutput,sep="\t",row.names=FALSE,quote=F);
